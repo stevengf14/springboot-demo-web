6656452db63bf35a609b8b671bba583196970d93
Steven <stevengf14@gmail.com>
2022-10-31 21:31:47 -0500

    Initial commit

---
+++ src/test/java/ec/learning/springboot/app/demo/SpringbootDemoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.learning.springboot.app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
