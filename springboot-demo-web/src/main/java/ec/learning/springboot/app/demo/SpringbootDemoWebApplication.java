e731764a292852793f77960df698cf38f33b1a95
Steven <stevengf14@gmail.com>
2022-11-01 22:14:09 -0500

    springboot-demo-web

---
+++ springboot-demo-web/src/main/java/ec/learning/springboot/app/demo/SpringbootDemoWebApplication.java
@@ -0,0 +1,13 @@
+package ec.learning.springboot.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoWebApplication.class, args);
+	}
+
+}
